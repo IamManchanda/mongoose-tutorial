b20ac9c86f8613d3b9cedd581ed4da1866e80f6d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-08 21:45:48 +0530

    Create user endpoint

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const User = require("./models/user");
 
 const { DATABASE_URI } = process.env;
 
-(async function initializeConnection() {
+const app = express();
+app.use(express.json());
+
+app.post("/users", async function createUser(req, res) {
+  const { name, email, role } = req.body;
   try {
+    const user = await User.create({ name, email, role });
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
+app.listen(5000, async function bootApp() {
+  try {
+    console.log("Server listening on http://localhost:5000");
     await mongoose.connect(DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -12,4 +31,4 @@ const { DATABASE_URI } = process.env;
   } catch (error) {
     console.log(error);
   }
-})();
+});
