8c58236b7d77d47850258c31c7888e9e773c1907
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-08 20:07:31 +0530

    Starting work related Project Initialization.

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { DATABASE_URI } = process.env;
+
+(async function initializeConnection() {
+  try {
+    await mongoose.connect(DATABASE_URI, {
+      useNewUrlParser: true,
+    });
+    console.log("Database connection established successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+})();
