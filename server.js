872aefcca718b6b5de29ee9112f720caf675177d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-09 01:41:14 +0530

    readUsers, updateUser, deleteUser & findUser endpoint

--- server.js
+++ server.js
@@ -26,6 +26,88 @@ app.post("/users", async function createUser(req, res) {
   }
 });
 
+app.get("/users", async function readUsers(_req, res) {
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
+app.put("/users/:id", async function updateUser(req, res) {
+  const { id } = req.params;
+  const { name, email, role } = req.body;
+  try {
+    let user;
+    try {
+      user = await User.findById(id);
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({
+        error: `User with id: ${id} not found`,
+      });
+    }
+    user.name = name;
+    user.email = email;
+    user.role = role;
+    await user.save();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
+app.delete("/users/:id", async function deleteUser(req, res) {
+  const { id } = req.params;
+  try {
+    try {
+      await User.findById(id);
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({
+        error: `User with id: ${id} not found`,
+      });
+    }
+    await User.findByIdAndDelete(id);
+    return res.status(200).json({
+      message: `User with id: ${id} deleted successfully`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
+app.get("/users/:id", async function findUser(req, res) {
+  const { id } = req.params;
+  try {
+    let user;
+    try {
+      user = await User.findById(id);
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({
+        error: `User with id: ${id} not found`,
+      });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+});
+
 app.listen(5000, async function bootApp() {
   try {
     console.log("Server listening on http://localhost:5000");
